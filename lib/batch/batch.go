1398e8457c4539dc04f922829f9e18ee819d1645
Rollan (rtm-golang) <rtm.golang@gmail.com>
2022-07-10 10:43:18 +0600

    Implement worker pool with channels and goroutines for users batch get

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,32 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	ids := make(chan int64, pool)
+	defer close(ids)
+
+	users := make(chan user, n)
+	defer close(users)
+
+	for p := int64(0); p < pool && p < n; p++ {
+		wg.Add(1)
+		go func(ids <-chan int64, users chan<- user, wg *sync.WaitGroup) {
+			defer wg.Done()
+			for i := range ids {
+				users <- getOne(i)
+			}
+		}(ids, users, &wg)
+	}
+
+	for i := int64(0); i < n; i++ {
+		ids <- i
+	}
+
+	for i := int64(0); i < n; i++ {
+		res = append(res, <-users)
+	}
+
+	return res
 }
